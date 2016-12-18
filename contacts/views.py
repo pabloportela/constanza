acf192de15adff275f3ae359e9316bdf3758503d
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2016-12-18 12:46:28 +0000

    Restricted GET method.

--- contacts/views.py
+++ contacts/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
 from .ContactsManager import ContactsManager
 
-
 @cache_page(60 * 5)
 def index(request):
+    if request.method != 'GET':
+        return(JsonResponse(status=501))
+
     try:
         m = ContactsManager()
         contacts = m.get()
