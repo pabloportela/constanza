a024fa3ea40360e202dab1fbfb00be5bcecb9103
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2016-12-19 21:18:30 +0000

    Tuples for a constant, instead of a list. Quote consistency, settling for single ones

--- contacts/ContactsManager.py
+++ contacts/ContactsManager.py
@@ -6,8 +6,8 @@ handle contacts date from some google drive.
 '''
 
 class ContactsManager:
-    url = "https://docs.google.com/spreadsheets/d/1A77-RWx7x8PK2uDm_1XlXyCy2ID9-9lhwix8wPDd5X0/pub?gid=0&single=true&output=csv"
-    headers = ['firstname', 'lastname', 'street', 'zip', 'city', 'image']
+    url = 'https://docs.google.com/spreadsheets/d/1A77-RWx7x8PK2uDm_1XlXyCy2ID9-9lhwix8wPDd5X0/pub?gid=0&single=true&output=csv'
+    headers = ('firstname', 'lastname', 'street', 'zip', 'city', 'image')
 
     def get(self):
         r = requests.get(self.url)
