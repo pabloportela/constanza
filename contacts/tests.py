a74d137f293756945db1b3c6e6b97c2b97eac040
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2016-12-20 21:33:45 +0000

    Added sample test.

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_contacts(self):
+        # Issue a GET request.
+        response = self.client.get('/contacts/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered json contains 4 contacts.
+        self.assertEqual(len(response.json()), 4)
