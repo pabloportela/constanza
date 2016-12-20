898d02956b3b89d534e583649484258efe8c0882
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2016-12-20 22:12:19 +0000

    Added tests.

--- contacts/tests.py
+++ contacts/tests.py
@@ -2,11 +2,11 @@ import unittest
 from django.test import Client
 
 class SimpleTest(unittest.TestCase):
+    """Makes sure the app works with and only with the http GET method"""
     def setUp(self):
-        # Every test needs a client.
         self.client = Client()
 
-    def test_contacts(self):
+    def test_get_contacts(self):
         # Issue a GET request.
         response = self.client.get('/contacts/')
 
@@ -15,3 +15,12 @@ class SimpleTest(unittest.TestCase):
 
         # Check that the rendered json contains 4 contacts.
         self.assertEqual(len(response.json()), 4)
+
+    def test_post_contacts(self):
+        response = self.client.post('/contacts/')
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_not_found(self):
+        response = self.client.post('/i_do_not_exist/')
+        self.assertEqual(response.status_code, 404)
+
